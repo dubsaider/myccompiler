e1a0a9dae053ff8f3ff708f924e21e0b4c979b25
Kostenkov Vladislav <44964427+dubsaider@users.noreply.github.com>
2021-07-09 02:02:55 +1000

    Add files via upload
    update Type, Value, hexToDecimal, octalToDecimal
    create tests

--- lexer.cpp
+++ lexer.cpp
@@ -1,28 +1,36 @@
 #include <cstdio>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "string"
 
 class Lexeme {
  private:
   int lineLexeme{};
-  int pos{};
+  int posLexeme{};
   std::string data;
+  std::string type;
+  std::string value;
  public:
   Lexeme() = default;
-  Lexeme(int lineLexeme_, int pos_, std::string data_)
-      : lineLexeme(lineLexeme_), pos(pos_), data(std::move(data_)) {}
+  Lexeme(int lineLexeme_, int pos_, std::string data_, std::string type_, std::string value_)
+      : lineLexeme(lineLexeme_),
+        posLexeme(pos_),
+        data(std::move(data_)),
+        type(std::move(type_)),
+        value(std::move(value_)) {}
   int GetLine() const {
     return lineLexeme;
   }
   int GetPos() const {
-    return pos;
+    return posLexeme;
   }
-//  int GetValue() const {
-//    return value;
-//  }
-//  const std::string &GetType() const {
-//    return type;
-//  }
+  const std::string &GetValue() const {
+    return value;
+  }
+  const std::string &GetType() const {
+    return type;
+  }
   const std::string &GetData() const {
     return data;
   }
@@ -32,14 +40,23 @@ class Lexeme {
 class Lexer {
  private:
   char buff[1024]{};
-  int len = 0;
+  size_t len = 0;
   int pos = 0;
   int posLexeme = 1;
   int lineLexeme = 1;
   int eof = 0;
   bool isPresenceLexeme = true;
   FILE *pathFile = nullptr;
-
+  const std::vector<std::string> keyword = {
+      "auto", "double", "int", "struct", "break", "else", "long", "switch", "case", "enum", "register", "typedef",
+      "char", "extern", "return", "union", "const", "float", "short", "unsigned", "continue", "for", "signed",
+      "void", "default", "goto", "sizeof", "volatile", "do", "if", "static", "while", "-1"};
+  const std::vector<std::pair<std::string, std::string>> operators = {
+      {"[", "LSBRACKET"}, {"]", "RSBRACKET"}, {"(", "LRBRACKET"}, {")", "RRBRACKET"}, {".", "DOT"}, {",", "COMMA"},
+      {"?", "QUESTION"}, {":", "COLON"}, {"~", "TILDE"}, {"-", "SUB"}, {"+", "ADD"}, {"&", "BITAND"}, {"*", "MULT"},
+      {"<", "LESS"}, {">", "MORE"}, {"=", "ASSING"}, {"|", "BITOP"}, {"!", "NOT"}, {"/", "DIV"}, {"%", "MOD"},
+      {"^", "XOR"}, {";", "SEMICOLON"}, {"{", "LFBRACKET"}, {"}", "RFBRACKET"},
+  };
   char _fgetc() {
     if (pos == len) {
       pos = 0;
@@ -52,11 +69,71 @@ class Lexer {
     return buff[pos++];
   }
 
+  std::string defineType(const std::string &text) {
+    std::string type;
+    if (std::find(keyword.begin(), keyword.end(), text) != keyword.end()) {
+      return "KEYWORD";
+    }
+    if (!isalnum(text[0])) {
+      return "OPERATOR";
+    }
+    for (char i : text) {
+      if (text[0] == '0' && text[1] == 'x') {
+        break;
+      }
+      if (!isdigit(i)) {
+        return "IDENTIFIER";
+      }
+    }
+    return "INTEGER";
+  }
+
+  static std::string hexToDecimal(const std::string &text) {
+    unsigned long long result = 0;
+    for (char i : text) {
+      result *= 16;
+      int num = tolower(i);
+      if (num >= '0' && num <= '9') {
+        result += num - 48;
+      } else if (num <= 'f' && num >= 'a') {
+        result += num - 87;
+      }
+    }
+    return std::to_string(result);
+  }
+  static std::string octToDecimal(const std::string &text) {
+    int result = 0;
+    for (char i : text) {
+      result *= 8;
+      result += i - 48;
+    }
+    return std::to_string(result);
+  }
+  std::string defineValue(const std::string &text, const std::string &type) {
+    if (type == "INTEGER") {
+      if (text[1] == 'x') {
+        return hexToDecimal(text);
+      }
+      if (text[0] == '0') {
+        return octToDecimal(text);
+      }
+    }
+    if (type == "OPERATOR") {
+      for (const auto &item: operators) {
+        if (text == item.first) {
+          return item.second;
+        }
+      }
+    }
+    return text;
+  }
+
  public:
   explicit Lexer(FILE *file) : pathFile(file) {}
 
   Lexeme takeLexeme() {
     std::string text;
+    std::string type;
     isPresenceLexeme = false;
     char symbol;
     int currentPosLexeme = posLexeme;
@@ -69,31 +146,25 @@ class Lexer {
         symbol = _fgetc();
         posLexeme++;
       }
-      if (!isalnum(symbol) && symbol != '\n') {
+      if (!isalnum(symbol)) {
         isPresenceLexeme = true;
         text += symbol;
       }
-      if (symbol == '\n' || symbol == -1) {
+      if (symbol == '\n') {
         currentLineLexeme = lineLexeme++;
         posLexeme = 1;
       } else {
         posLexeme++;
       }
     }
-//    while (true) {
-//      symbol = _fgetc();
-//      if (isalnum(symbol)) {
-//        text += symbol;
-//        symbol = _fgetc();
-//        posLexeme++;
-//      } else if (symbol > 8 && symbol < 13 || symbol == 32){
-//        symbol = _fgetc();
-//        symbol == 10 ? posLexeme = 1, currentLineLexeme = lineLexeme++ : posLexeme++;
-//      }
-//    }
-    Lexeme lexeme(currentLineLexeme, currentPosLexeme, text);
+    if (text[text.length() - 1] > 9 && text[text.length() - 1] < 13 || text[text.length() - 1] == 32
+        || text[text.length() - 1] == -1) {
+      text.pop_back();
+    }
+    type = defineType(text);
+    Lexeme lexeme(currentLineLexeme, currentPosLexeme, text, type, defineValue(text, type));
     return lexeme;
-    // TODO type, true value, operators
+    // TODO true value
   }
   int GetEof() const {
     return eof;
--- main.cpp
+++ main.cpp
@@ -2,24 +2,180 @@
 #include <cstdio>
 #include "lexer.cpp"
 
-int main(int argc, char *argv[]) {
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
-  FILE *file = nullptr;
+void testBody(){
 
-  std::cout << argv[1] << std::endl;
+}
 
-  if (argc < 4) {
-    file = fopen(argv[1], "r");
-  }
-
-  std::cout << file << '\n';
-
-  Lexer lexer(file);
-
+TEST_CASE("Empty file") {
+  FILE *inFile = fopen("parser/1.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::ofstream outFile;
   while (!lexer.GetEof()) {
     Lexeme lexeme = lexer.takeLexeme();
-    std::cout << '(' << lexeme.GetLine() << ':' << lexeme.GetPos() << ") " << lexeme.GetData() << "\n";
+    REQUIRE(lexeme.GetLine() == 1);
+    REQUIRE(lexeme.GetPos() == 1);
+    REQUIRE(lexeme.GetType() == "OPERATOR");
+    REQUIRE(lexeme.GetValue().empty());
+    REQUIRE(lexeme.GetData().empty());
   }
-
-  return 0;
 }
+TEST_CASE("File with space") {
+  FILE *inFile = fopen("parser/2.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::ofstream outFile;
+  while (!lexer.GetEof()) {
+    Lexeme lexeme = lexer.takeLexeme();
+    REQUIRE(lexeme.GetLine() == 1);
+    REQUIRE(lexeme.GetPos() == 1);
+    REQUIRE(lexeme.GetType() == "OPERATOR");
+    REQUIRE(lexeme.GetValue().empty());
+    REQUIRE(lexeme.GetData().empty());
+  }
+}
+TEST_CASE("File with keyword") {
+  FILE *inFile = fopen("parser/3.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::fstream outFile("parser/3.out");
+  while (!lexer.GetEof()) {
+    int line, pos;
+    std::string value, type, data;
+    Lexeme lexeme = lexer.takeLexeme();
+    if (lexeme.GetData().length() != 0) {
+      outFile >> line >> pos >> type >> value >> data;
+      REQUIRE(lexeme.GetLine() == line);
+      REQUIRE(lexeme.GetPos() == pos);
+      REQUIRE(lexeme.GetType() == type);
+      REQUIRE(lexeme.GetValue() == value);
+      REQUIRE(lexeme.GetData() == data);
+    }
+  }
+}
+TEST_CASE("File with operator") {
+  FILE *inFile = fopen("parser/4.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::fstream outFile("parser/4.out");
+  while (!lexer.GetEof()) {
+    int line, pos;
+    std::string value, type, data;
+    Lexeme lexeme = lexer.takeLexeme();
+    if (lexeme.GetData().length() != 0) {
+      outFile >> line >> pos >> type >> value >> data;
+      REQUIRE(lexeme.GetLine() == line);
+      REQUIRE(lexeme.GetPos() == pos);
+      REQUIRE(lexeme.GetType() == type);
+      REQUIRE(lexeme.GetValue() == value);
+      REQUIRE(lexeme.GetData() == data);
+    }
+  }
+}
+TEST_CASE("File with hexNum") {
+  FILE *inFile = fopen("parser/5.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::fstream outFile("parser/5.out");
+  while (!lexer.GetEof()) {
+    int line, pos;
+    std::string value, type, data;
+    Lexeme lexeme = lexer.takeLexeme();
+    if (lexeme.GetData().length() != 0) {
+      outFile >> line >> pos >> type >> value >> data;
+      REQUIRE(lexeme.GetLine() == line);
+      REQUIRE(lexeme.GetPos() == pos);
+      REQUIRE(lexeme.GetType() == type);
+      REQUIRE(lexeme.GetValue() == value);
+      REQUIRE(lexeme.GetData() == data);
+    }
+  }
+}
+
+TEST_CASE("File with octalNum") {
+  FILE *inFile = fopen("parser/6.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::fstream outFile("parser/6.out");
+  while (!lexer.GetEof()) {
+    int line, pos;
+    std::string value, type, data;
+    Lexeme lexeme = lexer.takeLexeme();
+    if (lexeme.GetData().length() != 0) {
+      outFile >> line >> pos >> type >> value >> data;
+      REQUIRE(lexeme.GetLine() == line);
+      REQUIRE(lexeme.GetPos() == pos);
+      REQUIRE(lexeme.GetType() == type);
+      REQUIRE(lexeme.GetValue() == value);
+      REQUIRE(lexeme.GetData() == data);
+    }
+  }
+}
+TEST_CASE("File with identifierNum") {
+  FILE *inFile = fopen("parser/7.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::fstream outFile("parser/7.out");
+  while (!lexer.GetEof()) {
+    int line, pos;
+    std::string value, type, data;
+    Lexeme lexeme = lexer.takeLexeme();
+    if (lexeme.GetData().length() != 0) {
+      outFile >> line >> pos >> type >> value >> data;
+      REQUIRE(lexeme.GetLine() == line);
+      REQUIRE(lexeme.GetPos() == pos);
+      REQUIRE(lexeme.GetType() == type);
+      REQUIRE(lexeme.GetValue() == value);
+      REQUIRE(lexeme.GetData() == data);
+    }
+  }
+}
+TEST_CASE("File with identifierAlpha") {
+  FILE *inFile = fopen("parser/8.in", "r");
+  Lexer lexer(inFile);
+  std::vector<Lexeme> lexemes;
+  std::fstream outFile("parser/8.out");
+  while (!lexer.GetEof()) {
+    int line, pos;
+    std::string value, type, data;
+    Lexeme lexeme = lexer.takeLexeme();
+    if (lexeme.GetData().length() != 0) {
+      outFile >> line >> pos >> type >> value >> data;
+      REQUIRE(lexeme.GetLine() == line);
+      REQUIRE(lexeme.GetPos() == pos);
+      REQUIRE(lexeme.GetType() == type);
+      REQUIRE(lexeme.GetValue() == value);
+      REQUIRE(lexeme.GetData() == data);
+    }
+  }
+}
+
+//int main(int argc, char *argv[]) {
+//
+//  FILE *file = nullptr;
+//
+//  std::cout << argv[1] << std::endl;
+//
+//  if (argc < 4) {
+//    file = fopen(argv[1], "r");
+//  }
+//
+//  std::cout << file << '\n';
+//
+//  Lexer lexer(file);
+//
+//  std::vector<Lexeme> lexemes;
+//
+//  while (!lexer.GetEof()) {
+//    Lexeme lexeme = lexer.takeLexeme();
+//    if (lexeme.GetData().length() != 0) {
+//      lexemes.push_back(lexeme);
+//      std::cout << lexeme.GetLine() << ' ' << lexeme.GetPos() << ' ' << lexeme.GetType() << ' '
+//                << lexeme.GetValue() << ' ' << lexeme.GetData() << "\n";
+//    }
+//  }
+//  return 0;
+//}
